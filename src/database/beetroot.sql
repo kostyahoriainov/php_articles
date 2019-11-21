a878abe473e47093d8b0f7e1e73653543429bfa0
kostyahoriainov <konstiantyn.horiainov@gmail.com>
2019-11-21 20:35:23 +0200

    table

--- src/database/beetroot.sql
+++ src/database/beetroot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: mysql:3306
--- Время создания: Ноя 20 2019 г., 17:31
+-- Время создания: Ноя 21 2019 г., 18:33
 -- Версия сервера: 5.7.28
 -- Версия PHP: 7.2.23
 
@@ -29,15 +29,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `text` text CHARACTER SET utf8 NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+                            `id` int(11) NOT NULL,
+                            `name` varchar(64) CHARACTER SET utf8 NOT NULL,
+                            `text` text CHARACTER SET utf8 NOT NULL,
+                            `category_id` int(11) NOT NULL,
+                            `user_id` int(11) NOT NULL,
+                            `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+                            `status` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 
 -- --------------------------------------------------------
 
@@ -46,8 +46,8 @@ CREATE TABLE `articles` (
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL
+                              `id` int(11) NOT NULL,
+                              `name` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -69,13 +69,23 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `article_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `text` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+                            `id` int(11) NOT NULL,
+                            `article_id` int(11) NOT NULL,
+                            `user_id` int(11) NOT NULL,
+                            `text` varchar(255) CHARACTER SET utf8 NOT NULL,
+                            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `test`
+--
+
+CREATE TABLE `test` (
+    `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -84,13 +94,13 @@ CREATE TABLE `comments` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(32) CHARACTER SET utf8 NOT NULL,
-  `last_name` varchar(32) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `login` varchar(32) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(32) CHARACTER SET utf8 NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+                         `id` int(11) NOT NULL,
+                         `first_name` varchar(32) CHARACTER SET utf8 NOT NULL,
+                         `last_name` varchar(32) CHARACTER SET utf8 NOT NULL,
+                         `email` varchar(64) CHARACTER SET utf8 NOT NULL,
+                         `login` varchar(32) CHARACTER SET utf8 NOT NULL,
+                         `password` varchar(32) CHARACTER SET utf8 NOT NULL,
+                         `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -101,9 +111,9 @@ CREATE TABLE `users` (
 --
 
 CREATE TABLE `user_article_rating` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `article_id` int(11) NOT NULL
+                                       `id` int(11) NOT NULL,
+                                       `user_id` int(11) NOT NULL,
+                                       `article_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -115,38 +125,38 @@ CREATE TABLE `user_article_rating` (
 -- Индексы таблицы `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `user_id` (`user_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `category_id` (`category_id`),
+    ADD KEY `user_id` (`user_id`);
 
 --
 -- Индексы таблицы `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `article_id` (`article_id`),
-  ADD KEY `user_id` (`user_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `article_id` (`article_id`),
+    ADD KEY `user_id` (`user_id`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login` (`login`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `login` (`login`);
 
 --
 -- Индексы таблицы `user_article_rating`
 --
 ALTER TABLE `user_article_rating`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_article_rating_ibfk_1` (`article_id`),
-  ADD KEY `user_article_rating_ibfk_2` (`user_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `user_article_rating_ibfk_1` (`article_id`),
+    ADD KEY `user_article_rating_ibfk_2` (`user_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -156,31 +166,31 @@ ALTER TABLE `user_article_rating`
 -- AUTO_INCREMENT для таблицы `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT для таблицы `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT для таблицы `user_article_rating`
 --
 ALTER TABLE `user_article_rating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -190,22 +200,22 @@ ALTER TABLE `user_article_rating`
 -- Ограничения внешнего ключа таблицы `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+    ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+    ADD CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+    ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `user_article_rating`
 --
 ALTER TABLE `user_article_rating`
-  ADD CONSTRAINT `user_article_rating_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_article_rating_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+    ADD CONSTRAINT `user_article_rating_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `user_article_rating_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
